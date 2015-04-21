fc31474d635a8b8540dfa7f47bdfdf8b034c5936
avijayak <avijayak@visa.com>
2015-04-21 15:14:30 +0530

    removed warning

--- src/main/java/com/psk/pms/validator/ProjectDetailValidator.java
+++ src/main/java/com/psk/pms/validator/ProjectDetailValidator.java
@@ -1,7 +1,6 @@
 package com.psk.pms.validator;
 
 import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.psk.pms.model.ProjectDetail;
