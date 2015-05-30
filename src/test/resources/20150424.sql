ecab9763ea139e528e151242b702048b1450e2de
unknown <ajithkv@MAA-H8CX5Z1.ant.amazon.com>
2015-05-30 22:03:54 +0530

    EMD save form data issue due to table structure and constraints resolved

--- src/test/resources/20150424.sql
+++ src/test/resources/20150424.sql
@@ -1,8 +1,10 @@
+CREATE DATABASE  IF NOT EXISTS `pms` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `pms`;
 -- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
 --
 -- Host: localhost    Database: pms
 -- ------------------------------------------------------
--- Server version	5.6.24-log
+-- Server version	5.7.7-rc-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +18,42 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `emddetail`
+--
+
+DROP TABLE IF EXISTS `emddetail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `emddetail` (
+  `ProjId` int(10) NOT NULL,
+  `SubProjId` int(10) DEFAULT NULL,
+  `EmdAmount` decimal(15,2) NOT NULL,
+  `EmdStartDate` datetime NOT NULL,
+  `EmdEndDate` datetime NOT NULL,
+  `EmdType` varchar(20) NOT NULL,
+  `BGNumber` varchar(20) NOT NULL,
+  `EmdPeriod` int(10) NOT NULL,
+  `EmdExtensionDate` datetime NOT NULL,
+  `EmdLedgerNumber` varchar(20) NOT NULL,
+  `SubProjectEmd` varchar(20) DEFAULT '0',
+  `LastUpdatedBy` varchar(30) NOT NULL,
+  `LastUpdatedAt` datetime NOT NULL,
+  `EmdId` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`EmdId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `emddetail`
+--
+
+LOCK TABLES `emddetail` WRITE;
+/*!40000 ALTER TABLE `emddetail` DISABLE KEYS */;
+INSERT INTO `emddetail` VALUES (3,NULL,12.00,'2015-05-30 00:00:00','2015-05-30 00:00:00','Bank Guarantee','123',2,'2015-05-30 00:00:00','12','0','tkumar','2015-05-30 21:31:05',1),(3,5,12.00,'2015-05-30 00:00:00','2015-05-30 00:00:00','Bank Guarantee','123',2,'2015-05-30 00:00:00','12','0','tkumar','2015-05-30 21:31:43',2);
+/*!40000 ALTER TABLE `emddetail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `employee`
 --
 
@@ -27,10 +65,10 @@ CREATE TABLE `employee` (
   `empPassword` varchar(60) NOT NULL,
   `empFirstName` varchar(40) NOT NULL,
   `empLastName` varchar(40) NOT NULL,
-  `empAddress` varchar(100) NOT NULL,
+  `empAddress` varchar(100) DEFAULT NULL,
   `empGender` varchar(10) NOT NULL,
   `empMobNum` varchar(20) NOT NULL,
-  `empMail` varchar(50) NOT NULL,
+  `empMail` varchar(50) DEFAULT NULL,
   `empMotherName` varchar(30) NOT NULL,
   `enabled` tinyint(4) NOT NULL DEFAULT '1',
   `empTeam` varchar(30) DEFAULT NULL,
@@ -61,25 +99,27 @@ CREATE TABLE `project` (
   `AliasProjName` varchar(50) NOT NULL,
   `AgreementNum` varchar(50) NOT NULL,
   `CERNum` varchar(30) NOT NULL,
-  `Amount` numeric(15,2) NOT NULL,
+  `Amount` decimal(15,2) NOT NULL,
   `ContractorName` varchar(50) NOT NULL,
   `ContractorAdd` tinytext NOT NULL,
-  `AgreementValue` numeric(15,2) NOT NULL,
-  `TenderValue` numeric(15,2) NOT NULL,
-  `ContractorValue` numeric(15,2) NOT NULL,
-  `ExcessInAmount` numeric(15,2) NOT NULL,
-  `ExcessInPercentage` numeric(15,2) NOT NULL,
+  `AgreementValue` decimal(15,2) NOT NULL,
+  `TenderValue` decimal(15,2) NOT NULL,
+  `ContractorValue` decimal(15,2) NOT NULL,
+  `ExcessInAmount` decimal(15,2) NOT NULL,
+  `ExcessInPercentage` decimal(15,2) NOT NULL,
   `TenderDate` datetime NOT NULL,
-  `AddSecurityDeposit` decimal(15,2) not null,
-  `LastUpdatedBy` varchar(30) not null,
-  `LastUpdatedAt` datetime not null,
-  `AgreementDate` datetime NULL,
-  `CommencementDate` datetime NULL,
-  `CompletedDate` datetime NULL,
-  `AgreementPeriod` int(10) NULL,
+  `EmdEndDate` datetime NOT NULL,
+  `EmdAmount` decimal(15,2) NOT NULL,
+  `AddSecurityDeposit` decimal(15,2) NOT NULL,
+  `LastUpdatedBy` varchar(30) NOT NULL,
+  `LastUpdatedAt` datetime NOT NULL,
+  `AgreementDate` datetime DEFAULT NULL,
+  `CommencementDate` datetime DEFAULT NULL,
+  `CompletedDate` datetime DEFAULT NULL,
+  `AgreementPeriod` int(10) DEFAULT NULL,
   PRIMARY KEY (`ProjId`),
   UNIQUE KEY `AliasProjName_UNIQUE` (`AliasProjName`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,6 +128,7 @@ CREATE TABLE `project` (
 
 LOCK TABLES `project` WRITE;
 /*!40000 ALTER TABLE `project` DISABLE KEYS */;
+INSERT INTO `project` VALUES (3,'Life Insurance','LIC','12312','CER234',12345.00,'PSK','mount road, chennai',12345.00,12345.00,12345.00,12.00,12.00,'2015-05-28 00:00:00','2015-05-03 00:00:00',1234.00,123.00,'tkumar','2015-05-29 22:22:53','2015-05-27 00:00:00','2015-05-27 00:00:00','2015-05-27 00:00:00',2);
 /*!40000 ALTER TABLE `project` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -102,19 +143,19 @@ CREATE TABLE `projectdesc` (
   `ProjId` int(10) NOT NULL,
   `SubProjId` int(10) NOT NULL,
   `WorkType` varchar(30) NOT NULL,
-  `QuantityInFig` numeric(15,2) NOT NULL,
+  `QuantityInFig` decimal(15,2) NOT NULL,
   `QuantityInWords` varchar(50) NOT NULL,
   `Description` text NOT NULL,
   `AliasDescription` varchar(100) NOT NULL,
-  `RateInFig` numeric(15,2) NOT NULL,
+  `RateInFig` decimal(15,2) NOT NULL,
   `RateInWords` varchar(50) NOT NULL,
-  `Amount` numeric(15,2) NOT NULL,
-  `LastUpdatedBy` varchar(30) not null,
-  `LastUpdatedAt` datetime not null,
+  `Amount` decimal(15,2) NOT NULL,
+  `LastUpdatedBy` varchar(30) NOT NULL,
+  `LastUpdatedAt` datetime NOT NULL,
   `ProjDescId` int(10) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`ProjDescId`),
   KEY `AliasSubProjName` (`SubProjId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,6 +164,7 @@ CREATE TABLE `projectdesc` (
 
 LOCK TABLES `projectdesc` WRITE;
 /*!40000 ALTER TABLE `projectdesc` DISABLE KEYS */;
+INSERT INTO `projectdesc` VALUES (3,5,'Main Work',123.00,'one two three','desc work','LICF1MW1',123.00,'312',122.00,'tkumar','2015-05-30 16:05:49',2);
 /*!40000 ALTER TABLE `projectdesc` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,25 +181,25 @@ CREATE TABLE `subproject` (
   `AliasSubProjName` varchar(50) NOT NULL,
   `AgreementNum` varchar(50) NOT NULL,
   `CERNum` varchar(30) NOT NULL,
-  `Amount` numeric(15,2) NOT NULL,
+  `Amount` decimal(15,2) NOT NULL,
   `ContractorName` varchar(50) NOT NULL,
   `ContractorAdd` tinytext NOT NULL,
-  `AgreementValue` numeric(15,2) NOT NULL,
-  `TenderValue` numeric(15,2) NOT NULL,
-  `ContractorValue` numeric(15,2) NOT NULL,
-  `ExcessInAmount` numeric(15,2) NOT NULL,
-  `ExcessInPercentage` numeric(15,2) NOT NULL,
-  `SubAddSecurityDeposit` decimal(15,2) not null,
+  `AgreementValue` decimal(15,2) NOT NULL,
+  `TenderValue` decimal(15,2) NOT NULL,
+  `ContractorValue` decimal(15,2) NOT NULL,
+  `ExcessInAmount` decimal(15,2) NOT NULL,
+  `ExcessInPercentage` decimal(15,2) NOT NULL,
+  `SubAddSecurityDeposit` decimal(15,2) NOT NULL,
   `TenderDate` datetime NOT NULL,
-  `AgreementDate` datetime NULL,
-  `CommencementDate` datetime NULL,
-  `CompletedDate` datetime NULL,
-  `AgreementPeriod` int(10) NULL,
-  `LastUpdatedBy` varchar(30) not null,
-  `LastUpdatedAt` datetime not null,
+  `AgreementDate` datetime DEFAULT NULL,
+  `CommencementDate` datetime DEFAULT NULL,
+  `CompletedDate` datetime DEFAULT NULL,
+  `AgreementPeriod` int(10) DEFAULT NULL,
+  `LastUpdatedBy` varchar(30) NOT NULL,
+  `LastUpdatedAt` datetime NOT NULL,
   `ProjId` int(10) NOT NULL,
   PRIMARY KEY (`SubProjId`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -166,6 +208,7 @@ CREATE TABLE `subproject` (
 
 LOCK TABLES `subproject` WRITE;
 /*!40000 ALTER TABLE `subproject` DISABLE KEYS */;
+INSERT INTO `subproject` VALUES (5,'LIC 1st floor','LICF1','12212','3131',3123.00,'psk','mt road',3123.00,3213.00,12321.00,31.00,32.00,12312.00,'2015-05-30 00:00:00','2015-05-30 00:00:00','2015-05-30 00:00:00','2015-05-30 00:00:00',1,'tkumar','2015-05-30 16:00:47',3);
 /*!40000 ALTER TABLE `subproject` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -225,30 +268,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-05-05 21:17:37
-
---
--- Dumping data for table `emddetail`
---
-
-
-DROP TABLE IF EXISTS `emddetail`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `emddetail` (
-  `EmdId` int(10) NOT NULL  AUTO_INCREMENT,
-  `ProjId` int(10) NOT NULL,
-  `SubProjId` int(10) NULL,
-  `EmdAmount` numeric(15,2) NOT NULL,
-  `EmdStartDate` datetime not null,
-  `EmdEndDate` datetime not null,
-  `EmdType` Varchar(20) NOT NULL,
-  `BGNumber` Varchar(20) NULL,
-  `EmdPeriod`int(10) NOT NULL,
-  `EmdExtensionDate` datetime NULL,
-  `EmdLedgerNumber` Varchar(20) NOT NULL,
-  `LastUpdatedBy` varchar(30) not null,
-  `LastUpdatedAt` datetime not null,
-   PRIMARY KEY (`EmdId`)
-   );
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Dump completed on 2015-05-30 21:59:24
