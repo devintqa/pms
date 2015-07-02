2c21e465ff496b7f2b77b4b291a6076d1a9052f8
devintqa <devint123>
2015-07-03 00:02:25 +0530

    Refactoring a bit

--- src/main/java/com/psk/pms/controller/ItemController.java
+++ src/main/java/com/psk/pms/controller/ItemController.java
@@ -1,15 +1,30 @@
 package com.psk.pms.controller;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+import com.psk.pms.model.DescItemDetail;
 import com.psk.pms.model.Item;
+import com.psk.pms.model.DescItemDetail.ItemDetail;
 import com.psk.pms.service.ProjectService;
 
 @Controller
@@ -30,6 +45,38 @@ public class ItemController {
 		item.setEmployeeId(employeeId);
 		model.addAttribute("itemForm", item);
 		return "BuildItem";
+	}
+	
+	@RequestMapping(value = "/emp/myview/buildProjectDesc/loadProjDescItems.do")
+	public String loadProjDescItems(Model model, @RequestParam String projDescSerial, 
+								@RequestParam String projId, @RequestParam String subProjId, 
+								@RequestParam String projDescId, @RequestParam String employeeId) {
+		DescItemDetail descItemDetail = new  DescItemDetail();
+		descItemDetail.setProjId(new Integer(projId));
+		descItemDetail.setSubProjId(new Integer(subProjId));
+		descItemDetail.setProjDescId(new Integer(projDescId));
+		descItemDetail.setProjDescSerial(projDescSerial);
+		descItemDetail = projectService.getDataDescription(descItemDetail);
+		
+		Gson gson = new Gson();
+		JsonElement element = gson.toJsonTree(descItemDetail.getItemDetail(), new TypeToken<List<ItemDetail>>() {}.getType());
+		if (! element.isJsonArray()) {
+			
+		}
+		JsonArray jsonArray = element.getAsJsonArray();
+		descItemDetail.setProjDescItemDetail(jsonArray.toString());
+		descItemDetail.setEmployeeId(employeeId);
+		model.addAttribute("descItemForm", descItemDetail);
+		return "DescItem";
+	}
+	
+	@RequestMapping(value = "/emp/myview/buildProjectDesc/saveProjDescItems.do", method = RequestMethod.POST, consumes="application/json")
+	public @ResponseBody boolean saveProjDescItems(@RequestBody DescItemDetail descItemDetail) throws JsonParseException, JsonMappingException, IOException{
+		ObjectMapper mapper = new ObjectMapper();
+		List<com.psk.pms.model.DescItemDetail.ItemDetail> itemList = mapper.readValue(descItemDetail.getProjDescItemDetail(), mapper.getTypeFactory().constructCollectionType(List.class, com.psk.pms.model.DescItemDetail.ItemDetail.class));
+		descItemDetail.setItemDetail(itemList);
+		boolean status = projectService.insertDataDescription(descItemDetail);
+		return status;
 	}
 
 }
