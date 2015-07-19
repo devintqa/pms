473ba5b18af55590b0da2ed4bffc9560861f3077
devintqa <devint123>
2015-07-19 14:50:17 +0530

    Initial Bulk Upload Check-In

--- src/main/java/com/psk/pms/service/FileServiceImpl.java
+++ src/main/java/com/psk/pms/service/FileServiceImpl.java
@@ -1,15 +1,23 @@
 package com.psk.pms.service;
 
+import com.mysql.jdbc.StringUtils;
 import com.psk.pms.model.FileUpload;
+import com.psk.pms.model.ProjDescDetail;
 import com.psk.pms.model.ProjectDetail;
 import com.psk.pms.model.SubProjectDetail;
 import org.apache.log4j.Logger;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class FileServiceImpl implements FileService {
@@ -21,6 +29,113 @@ public class FileServiceImpl implements FileService {
     private SubProjectService subProjectService;
 
     private static final Logger LOGGER = Logger.getLogger(FileServiceImpl.class);
+    
+    @Override
+    public void saveProjectDescription(FileUpload fileUpload) throws IOException{
+        File files;
+        String saveDirectory;
+        ProjectDetail projectDetail = projectService.getProjectDocument(fileUpload.getAliasProjectName());
+        LOGGER.info("method = uploadFiles() , Alias Project Name" + projectDetail.getAliasName());
+        if (fileUpload.isSubProjectUpload()) {
+            SubProjectDetail subProjDetail = subProjectService.getSubProjectDocument(fileUpload.getAliasSubProjectName());
+            files = new File("C:\\PMS\\" + projectDetail.getAliasName() + "\\" + subProjDetail.getAliasSubProjName());
+            saveDirectory = "C:/PMS/" + projectDetail.getAliasName() + "/" + subProjDetail.getAliasSubProjName() + "/";
+        } else {
+            files = new File("C:\\PMS\\" + projectDetail.getAliasName());
+            saveDirectory = "C:/PMS/" + projectDetail.getAliasName() + "/";
+        }
+        List<MultipartFile> pmsFiles = fileUpload.getFiles();
+        if (null != pmsFiles && pmsFiles.size() > 0) {
+            for (MultipartFile multipartFile : pmsFiles) {      
+                try
+                {
+                    FileInputStream file = new FileInputStream(new File(saveDirectory + multipartFile.getOriginalFilename()));
+                    List<ProjDescDetail> detailList = new ArrayList<ProjDescDetail>();
+                    String aliasProjectName = "";
+                    String subAliasProjectName = "";
+                    //Create Workbook instance holding reference to .xlsx file
+                    XSSFWorkbook workbook = new XSSFWorkbook(file);
+         
+                    //Get first/desired sheet from the workbook
+                    XSSFSheet sheet = workbook.getSheetAt(0);
+         
+                    //Iterate through each rows one by one
+                    Iterator<Row> rowIterator = sheet.iterator();
+                    while (rowIterator.hasNext())
+                    {
+                        Row row = rowIterator.next();
+                        ProjDescDetail projDescDetail = new ProjDescDetail();
+                        if(row.getRowNum()==1 || row.getRowNum()==2){
+                            continue; //just skip the rows if row number is 1 or 2
+                         }
+                        //For each row, iterate through all the columns
+                        Iterator<Cell> cellIterator = row.cellIterator();
+                         
+                        while (cellIterator.hasNext())
+                        {
+                            Cell cell = cellIterator.next();
+                            //Check the cell type and format accordingly
+                            switch (cell.getCellType())
+                            {
+                                case Cell.CELL_TYPE_NUMERIC:
+                                	if (row.getRowNum()>=3 && cell.getColumnIndex() == 0 && row.getCell(1) != null)
+                                    {	
+                                		if(cell.getColumnIndex() == 0)projDescDetail.setSerialNumber(String.valueOf(cell.getNumericCellValue()));
+                                    }
+                                	if (row.getRowNum()>=3 && cell.getColumnIndex() == 2 && row.getCell(3) != null)
+                                    {	
+                                		if(cell.getColumnIndex() == 2)projDescDetail.setQuantityInFig(String.valueOf(cell.getNumericCellValue()));
+                                    }
+                                	break;
+                                case Cell.CELL_TYPE_STRING:
+                                	if (row.getRowNum()==0)
+                                    {
+                                		if(cell.getColumnIndex() == 1 && row.getCell(2) != null){
+                                			aliasProjectName = cell.getStringCellValue();
+                                    		break;
+                                		}
+                                		if(cell.getColumnIndex() == 3 && row.getCell(4) != null){
+                                			subAliasProjectName = cell.getStringCellValue();
+                                    		break;
+                                		}
+                                		
+                                    }
+                                	if (row.getRowNum()>=3)
+                                    {	
+                                		if(cell.getColumnIndex() == 0 && row.getCell(1) != null)projDescDetail.setSerialNumber(cell.getStringCellValue());
+                                		if(cell.getColumnIndex() == 1)projDescDetail.setWorkType(cell.getStringCellValue());
+                                		if(cell.getColumnIndex() == 3)projDescDetail.setQuantityInWords(cell.getStringCellValue());
+                                		if(cell.getColumnIndex() == 4)projDescDetail.setDescription(cell.getStringCellValue());
+                                		if(cell.getColumnIndex() == 5)projDescDetail.setAliasDescription(cell.getStringCellValue());
+                                    }
+                                    break;
+                            }
+                        }
+                        if (row.getRowNum()>=3 && !StringUtils.isNullOrEmpty(projDescDetail.getSerialNumber()))
+                        {
+                    		detailList.add(projDescDetail);
+                        }          
+                    }
+                    for(ProjDescDetail descDetail : detailList){
+                    	LOGGER.info(aliasProjectName);
+                    	LOGGER.info(subAliasProjectName);
+                    	LOGGER.info(descDetail.getSerialNumber());
+                    	LOGGER.info(descDetail.getWorkType());
+                    	LOGGER.info(descDetail.getQuantityInFig());
+                    	LOGGER.info(descDetail.getQuantityInWords());
+                    	LOGGER.info(descDetail.getDescription());
+                    	LOGGER.info(descDetail.getAliasDescription());
+                    }
+                    file.close();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                
+            }
+        }
+    }
 
     @Override
     public void uploadFiles(FileUpload fileUpload) throws IOException {
---
+++ src/main/webapp/WEB-INF/pages/UploadExcel.jsp
@@ -0,0 +1,112 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!doctype html>
+<html>
+<head>
+<title>PMS :: Create Project Description</title>
+
+<%@include file="Script.jsp" %>
+
+<script>
+    $(document)
+            .ready(
+                    function() {
+                      $("#showSubProject").hide();
+                  	  $('#subProjectUpload').change(function() {
+              	        if($(this).is(":checked")) {
+              			var aliasProjectName  = $('#projId').val();
+              			$.ajax({
+              				type : "GET",
+              				url : "getSubAliasProject.do",
+              				cache : false,
+              				data: "aliasProjectName="+aliasProjectName,
+              				success : function(response) {
+              					var options = '';
+              					if (response != null) {
+              						var obj = jQuery.parseJSON(response);
+              						var options = '';
+              						for ( var key in obj) {
+              							var attrName = key;
+              							var attrValue = obj[key];
+              							options = options + '<option value='+attrName+'>'
+              									+ attrValue + '</option>';
+              						}
+              						$('#aliasSubProjectName').html(options);
+              					}
+              				}
+              			});
+              				$("#showSubProject").show();
+              	        }else {
+              	        	$("#showSubProject").hide();
+              	        }
+              		}); 
+              	  $('#projId').change(function() {
+              		  $("#showSubProject").hide();
+              		  $('#subProjectUpload').attr('checked', false);
+              	  });
+
+              	  if($('#subProjectUpload').is(':checked')) {
+              	  		$("#showSubProject").show();
+              	  };
+ 
+                    });
+</script>
+</head>
+<body ng-app="sampleApp">
+	<header>
+		<jsp:include page="Header.jsp" />
+	</header>
+	<div>
+		<div>
+			<h2
+				style="text-align: left; font-family: arial; color: #007399; font-size: 14px;">${fileAdditionSuccessful}</h2>
+		</div>
+		<div>
+ 
+        <form:form method="post" action="saveProjectDescription.do"
+            modelAttribute="uploadForm" enctype="multipart/form-data">
+ 
+            <p>Select Project Description Data To Be Saved For The Project.</p>
+ 
+            <table id="fileTable">
+                            <tr>
+								<td>Alias Project Name <span id="colon">:</span>
+								</td>
+								<td><form:select path="aliasProjectName"
+										cssClass="inputText" id="projId" items="${aliasProjectList}" >
+									</form:select></td>
+								<td><form:errors path="aliasProjectName" cssClass="error" /></td>
+							</tr>
+							<tr>
+								<td>Upload For Sub Project? :</td>
+								<td><form:checkbox path="subProjectUpload" id="subProjectUpload"/></td>
+								<td><form:errors path="subProjectUpload" cssClass="error" /></td>
+							</tr>
+							<tr id="showSubProject">
+								<td>Sub Project Name <span id="colon">:</span>
+								</td>
+								<td><form:select path="aliasSubProjectName"
+										id="aliasSubProjectName" cssClass="inputText"  items="${subAliasProjectList}">
+										<c:if test="${projDescForm.subProjId gt '0'}">
+											<option value="${projDescForm.subProjId}" selected="selected">${projDescForm.aliasSubProjectName}</option>
+										</c:if>
+									</form:select></td>
+								<td><form:errors path="aliasSubProjectName"
+										cssClass="error" /></td>
+							</tr>
+                <tr>
+                    <td><form:input name="files[0]" type="file" path="files" id="files"/></td>
+                    <td><form:errors path="files" cssClass="error" /></td>
+                </tr>
+
+            </table>
+            <br />
+            <input type="submit" value="Upload" />
+        </form:form>
+        <br />
+    </div>
+    </div>
+</body>
+</html>
