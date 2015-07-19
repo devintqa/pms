4a8eb2c0e920277524848589441134ee2fd2df62
devintqa <devint123>
2015-07-19 15:32:44 +0530

    Excel Upload Functionality

--- src/main/java/com/psk/pms/service/FileServiceImpl.java
+++ src/main/java/com/psk/pms/service/FileServiceImpl.java
@@ -48,8 +48,6 @@ public class FileServiceImpl implements FileService {
                 {
                     FileInputStream file = new FileInputStream(new File(saveDirectory + multipartFile.getOriginalFilename()));
                     List<ProjDescDetail> detailList = new ArrayList<ProjDescDetail>();
-                    String aliasProjectName = "";
-                    String subAliasProjectName = "";
                     //Create Workbook instance holding reference to .xlsx file
                     XSSFWorkbook workbook = new XSSFWorkbook(file);
          
@@ -75,47 +73,30 @@ public class FileServiceImpl implements FileService {
                             switch (cell.getCellType())
                             {
                                 case Cell.CELL_TYPE_NUMERIC:
-                                	if (row.getRowNum()>=1 && cell.getColumnIndex() == 0 && row.getCell(1) != null)
+                                	if (cell.getColumnIndex() == 0 && row.getCell(1) != null)
                                     {	
                                 		if(cell.getColumnIndex() == 0)projDescDetail.setSerialNumber(String.valueOf(cell.getNumericCellValue()));
                                     }
-                                	if (row.getRowNum()>=1 && cell.getColumnIndex() == 2 && row.getCell(3) != null)
+                                	if (cell.getColumnIndex() == 2 && row.getCell(3) != null)
                                     {	
                                 		if(cell.getColumnIndex() == 2)projDescDetail.setQuantityInFig(String.valueOf(cell.getNumericCellValue()));
                                     }
                                 	break;
-                                case Cell.CELL_TYPE_STRING:
-                                	if (row.getRowNum()==0)
-                                    {
-                                		if(cell.getColumnIndex() == 1 && row.getCell(2) != null){
-                                			aliasProjectName = cell.getStringCellValue();
-                                    		break;
-                                		}
-                                		if(cell.getColumnIndex() == 3 && row.getCell(4) != null){
-                                			subAliasProjectName = cell.getStringCellValue();
-                                    		break;
-                                		}
-                                		
-                                    }
-                                	if (row.getRowNum()>=1)
-                                    {	
+                                case Cell.CELL_TYPE_STRING:	
                                 		if(cell.getColumnIndex() == 0 && row.getCell(1) != null)projDescDetail.setSerialNumber(cell.getStringCellValue());
                                 		if(cell.getColumnIndex() == 1)projDescDetail.setWorkType(cell.getStringCellValue());
                                 		if(cell.getColumnIndex() == 3)projDescDetail.setQuantityInWords(cell.getStringCellValue());
                                 		if(cell.getColumnIndex() == 4)projDescDetail.setDescription(cell.getStringCellValue());
                                 		if(cell.getColumnIndex() == 5)projDescDetail.setAliasDescription(cell.getStringCellValue());
-                                    }
                                     break;
                             }
                         }
-                        if (row.getRowNum()>=1 && !StringUtils.isNullOrEmpty(projDescDetail.getSerialNumber()))
+                        if (!StringUtils.isNullOrEmpty(projDescDetail.getSerialNumber()))
                         {
                     		detailList.add(projDescDetail);
                         }          
                     }
                     for(ProjDescDetail descDetail : detailList){
-                    	LOGGER.info(aliasProjectName);
-                    	LOGGER.info(subAliasProjectName);
                     	LOGGER.info(descDetail.getSerialNumber());
                     	LOGGER.info(descDetail.getWorkType());
                     	LOGGER.info(descDetail.getQuantityInFig());
