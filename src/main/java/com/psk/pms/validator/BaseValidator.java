b631a87423f96cc994a342569a2eb2a09df57066
devintqa <devint123>
2015-08-07 22:28:04 +0530

    Refactoring the code

--- src/main/java/com/psk/pms/validator/BaseValidator.java
+++ src/main/java/com/psk/pms/validator/BaseValidator.java
@@ -1,6 +1,22 @@
 package com.psk.pms.validator;
 
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.psk.pms.service.ProjectService;
+import com.psk.pms.service.SubProjectService;
+
 public class BaseValidator {
+	
+	@Autowired
+	protected ProjectService projectService;
+
+	@Autowired
+	protected SubProjectService subProjectService;
+	
+	private static final Logger LOGGER = Logger.getLogger(BaseValidator.class);
 	
 	 public static final String MOBILE_PATTERN = "[0-9]{10}";
 	 public static final String NAME_PATTERN = "^[a-zA-Z]{2,20}$";
@@ -8,5 +24,38 @@ public class BaseValidator {
 		   + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 	 String ID_PATTERN = "[0-9]+"; 
 	 String AMOUNT_PATTERN = "\\d+(?:\\.\\d+)?$";
+	 
+	 
+	    public String fetchProjectId(String aliasProjectName) {
+			LOGGER.info("method = fetchProjectId()");
+			Map<String, String> aliasProjectList = populateAliasProjectList();
+			for (Map.Entry<String, String> entry : aliasProjectList.entrySet()) {
+				if (entry.getValue().equalsIgnoreCase(aliasProjectName)) {
+					return entry.getKey();
+				}
+			}
+			return null;
+		}
+
+		public String fetchSubProjectId(String aliasSubProjName) {
+			LOGGER.info("method = fetchSubProjectId()");
+			Map<String, String> aliasSubProjectList = populateAliasSubProjectList();
+			for (Map.Entry<String, String> entry : aliasSubProjectList.entrySet()) {
+				if (entry.getValue().equalsIgnoreCase(aliasSubProjName)) {
+					return entry.getKey();
+				}
+			}
+			return null;
+		}
+		
+		public Map<String, String> populateAliasProjectList() {
+			Map<String, String> aliasProjectName = projectService.getAliasProjectNames();
+			return aliasProjectName;
+		}
+		
+		public Map<String, String> populateAliasSubProjectList() {
+			Map<String, String> subAliasProjectNames = subProjectService.getSubAliasProjectNames("");
+			return subAliasProjectNames;
+		}
 
 }
--- src/main/java/com/psk/pms/validator/SearchValidator.java
+++ src/main/java/com/psk/pms/validator/SearchValidator.java
@@ -2,24 +2,10 @@ package com.psk.pms.validator;
 
 import com.mysql.jdbc.StringUtils;
 import com.psk.pms.model.SearchDetail;
-import com.psk.pms.service.ProjectService;
-import com.psk.pms.service.SubProjectService;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-import java.util.Map;
-
 public class SearchValidator extends BaseValidator implements Validator{
-	
-	@Autowired
-	ProjectService projectService;
-
-	@Autowired
-	SubProjectService subProjectService;
-
-	private static final Logger LOGGER = Logger.getLogger(SearchValidator.class);
 
 	@Override
 	public boolean supports(Class<?> clazz) {
@@ -58,37 +44,6 @@ public class SearchValidator extends BaseValidator implements Validator{
 				searchDetail.setProjId(Integer.valueOf(projId));
 			}
 		}
-	}
-	
-	private String fetchProjectId(String aliasProjectName) {
-		LOGGER.info("method = fetchProjectId()");
-		Map<String, String> aliasProjectList = populateAliasProjectList();
-		for (Map.Entry<String, String> entry : aliasProjectList.entrySet()) {
-			if (entry.getValue().equalsIgnoreCase(aliasProjectName)) {
-				return entry.getKey();
-			}
-		}
-		return null;
-	}
-
-	private String fetchSubProjectId(String aliasSubProjName) {
-		LOGGER.info("method = fetchSubProjectId()");
-		Map<String, String> aliasSubProjectList = populateAliasSubProjectList();
-		for (Map.Entry<String, String> entry : aliasSubProjectList.entrySet()) {
-			if (entry.getValue().equalsIgnoreCase(aliasSubProjName)) {
-				return entry.getKey();
-			}
-		}
-		return null;
-	}
-	
-	public Map<String, String> populateAliasProjectList() {
-		Map<String, String> aliasProjectName = projectService.getAliasProjectNames();
-		return aliasProjectName;
-	}
-	public Map<String, String> populateAliasSubProjectList() {
-		Map<String, String> subAliasProjectNames = subProjectService.getSubAliasProjectNames("");
-		return subAliasProjectNames;
 	}
 
 }
