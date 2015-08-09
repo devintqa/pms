683990d2a7753ee4c4432be8f5230d52f7f99287
devintqa <devint123>
2015-08-09 21:14:01 +0530

    Refactoring the code

--- src/main/java/com/psk/pms/dao/ProjectDAOImpl.java
+++ src/main/java/com/psk/pms/dao/ProjectDAOImpl.java
@@ -12,18 +12,6 @@ import static com.psk.pms.dao.PmsMasterQuery.*;
 
 public class ProjectDAOImpl implements ProjectDAO {
 
-    @Autowired
-    private SubProjectDAO subProjectDAO;
-
-    @Autowired
-    private ProjectDescriptionDAO projectDescriptionDAO;
-
-    @Autowired
-    private EmdDAO emdDAO;
-
-    @Autowired
-    private ItemDAO itemDAO;
-
 
     @Qualifier("jdbcTemplate")
     @Autowired
@@ -210,10 +198,6 @@ public class ProjectDAOImpl implements ProjectDAO {
 
     @Override
     public void deleteProject(Integer projectId) {
-        itemDAO.deleteItemByProjectId(projectId);
-        emdDAO.deleteEmddetailByProjectId(projectId);
-        projectDescriptionDAO.deleteProjectDescriptionByProjectId(projectId);
-        subProjectDAO.deleteSubProjectByProjectId(projectId);
         int noOfRows = jdbcTemplate.update(DELETEPROJECTBYPROJECTID, new Object[]{projectId});
         LOGGER.info("method = deleteProject , Number of rows deleted : " + noOfRows + " projectId :" + projectId);
     }
