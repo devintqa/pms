1c5b70b478ccb0ca52ae7ba6a2fbd02b0a22d8c7
devintqa <devint123>
2015-09-12 18:49:14 +0530

    Fixing the sonar bugs

--- src/main/java/com/psk/pms/dao/PmsMasterQuery.java
+++ src/main/java/com/psk/pms/dao/PmsMasterQuery.java
@@ -2,25 +2,25 @@ package com.psk.pms.dao;
 
 public class PmsMasterQuery {
 
-    public static String NOOFFIELDDESCASSOCIATEDTOPROJECT = "SELECT count(*) FROM fieldprojectdesc WHERE ProjId = ?";
+    public static final String NOOFFIELDDESCASSOCIATEDTOPROJECT = "SELECT count(*) FROM fieldprojectdesc WHERE ProjId = ?";
 
-    public static String NOOFFIELDDESCASSOCIATEDTOSUBPROJECT = "SELECT count(*) FROM fieldprojectdesc WHERE SubProjId = ?";
+    public static final String NOOFFIELDDESCASSOCIATEDTOSUBPROJECT = "SELECT count(*) FROM fieldprojectdesc WHERE SubProjId = ?";
 
-    public static String CREATEPROJECTFIELDDESCRIPTION = "insert into fieldprojectdesc select * from projectdesc where ProjId = ? and subProjId is null";
+    public static final String CREATEPROJECTFIELDDESCRIPTION = "insert into fieldprojectdesc select * from projectdesc where ProjId = ? and subProjId is null";
 
-    public static String CREATESUBPROJECTFIELDDESCRIPTION = "insert into fieldprojectdesc select * from projectdesc where SubProjId = ?;";
+    public static final String CREATESUBPROJECTFIELDDESCRIPTION = "insert into fieldprojectdesc select * from projectdesc where SubProjId = ?;";
 
-    public static String DELETEPROJECTFIELDDESCRIPTION = "delete from fieldprojectdesc where ProjId = ? and SubProjId is null";
+    public static final String DELETEPROJECTFIELDDESCRIPTION = "delete from fieldprojectdesc where ProjId = ? and SubProjId is null";
 
-    public static String DELETESUBPROJECTFIELDDESCRIPTION = "delete from fieldprojectdesc where SubProjId = ?";
+    public static final String DELETESUBPROJECTFIELDDESCRIPTION = "delete from fieldprojectdesc where SubProjId = ?";
 
-    public static String DELETEPROJECTFIELDDESCRIPTIONITEM = "delete from fieldprojdescitem where ProjId = ? and SubProjId = 0";
+    public static final String DELETEPROJECTFIELDDESCRIPTIONITEM = "delete from fieldprojdescitem where ProjId = ? and SubProjId = 0";
 
-    public static String DELETESUBPROJECTFIELDDESCRIPTIONITEM = "delete from fieldprojdescitem where SubProjId = ?";
+    public static final String DELETESUBPROJECTFIELDDESCRIPTIONITEM = "delete from fieldprojdescitem where SubProjId = ?";
 
-    public static String CREATEPROJECTFIELDDESCRIPTIONITEM = "insert into fieldprojdescitem select * from projdescitem where ProjId = ? and subProjId =0";
+    public static final String CREATEPROJECTFIELDDESCRIPTIONITEM = "insert into fieldprojdescitem select * from projdescitem where ProjId = ? and subProjId =0";
 
-    public static String CREATESUBPROJECTFIELDDESCRIPTIONITEM = "insert into fieldprojdescitem select * from projdescitem where SubProjId = ?";
+    public static final String CREATESUBPROJECTFIELDDESCRIPTIONITEM = "insert into fieldprojdescitem select * from projdescitem where SubProjId = ?";
 
     public static final String CREATE_DEPOSIT_DETAIL = "INSERT INTO depositdetail (ProjId , SubProjId , DepositAmount , DepositStartDate ,DepositEndDate ,DepositType, BGNumber ,"
 			+ "DepositPeriod,DepositExtensionDate,DepositLedgerNumber, LastUpdatedBy,LastUpdatedAt,DepositSubmitter,DepositDetail,DepositStatus,DepositRecievedDate,DepositRecievedNote)"
@@ -64,84 +64,84 @@ public class PmsMasterQuery {
 			+ "    left join subproject as s on e.SubProjId=s.SubProjId where e.SubProjId=?";
 
 
-	public static String projDescDetailQuery = "SELECT ProjId, SubProjId, SerialNumber, WorkType, Quantity, Metric,"
+	public static final String projDescDetailQuery = "SELECT ProjId, SubProjId, SerialNumber, WorkType, Quantity, Metric,"
 			+ "Description, AliasDescription, PricePerQuantity, TotalCost, ProjDescId FROM projectdesc";
 
-	public static String compareDataQuery = "SELECT distinct p.SerialNumber, p.Quantity, p.Metric, p.AliasDescription, p.PricePerQuantity, p.TotalCost, pp.PricePerQuantity as DeptPricePerQuantity, pp.TotalCost as DeptTotalCost "
+	public static final String compareDataQuery = "SELECT distinct p.SerialNumber, p.Quantity, p.Metric, p.AliasDescription, p.PricePerQuantity, p.TotalCost, pp.PricePerQuantity as DeptPricePerQuantity, pp.TotalCost as DeptTotalCost "
 			+ "FROM projectdesc p INNER JOIN quotedprojectdesc pp on p.ProjId = pp.ProjId "
 			+ "and p.AliasDescription = pp.AliasDescription";
 
-	public static String projDescDetail = "SELECT d.ProjId, d.SubProjId, d.SerialNumber, d.WorkType, d.Quantity, d.Metric,"
+	public static final String projDescDetail = "SELECT d.ProjId, d.SubProjId, d.SerialNumber, d.WorkType, d.Quantity, d.Metric,"
 			+ "d.Description, d.AliasDescription, d.PricePerQuantity, d.TotalCost, d.ProjDescId";
 
-	public static String baseDescDetail = "SELECT BaseDescId, WorkType, Metric, Quantity, PricePerQuantity, LastUpdatedBy, LastUpdatedAt, Description, BaseDescription FROM basedesc";
+	public static final String baseDescDetail = "SELECT BaseDescId, WorkType, Metric, Quantity, PricePerQuantity, LastUpdatedBy, LastUpdatedAt, Description, BaseDescription FROM basedesc";
 	
-	public static String deleteProjDescDetailQuery = "DELETE FROM projectdesc where ProjDescId = ?";
+	public static final String deleteProjDescDetailQuery = "DELETE FROM projectdesc where ProjDescId = ?";
 
-	public static String DELETEPROJDESCITEMBYPROJECTID = "DELETE FROM projdescitem WHERE ProjId = ?";
+	public static final String DELETEPROJDESCITEMBYPROJECTID = "DELETE FROM projdescitem WHERE ProjId = ?";
 
-	public static String DELETEPROJDESCITEMBYSUBPROJECTID = "DELETE FROM  projdescitem WHERE SubProjId = ?";
+	public static final String DELETEPROJDESCITEMBYSUBPROJECTID = "DELETE FROM  projdescitem WHERE SubProjId = ?";
 
-	public static String DELETEPROJDESCAITEMBYPROJECTDESCID = "DELETE FROM projdescitem WHERE ProjDescId = ?";
+	public static final String DELETEPROJDESCAITEMBYPROJECTDESCID = "DELETE FROM projdescitem WHERE ProjDescId = ?";
 
-	public static String DELETEPROJDESCAITEMBYPROJECTDESCITEMID = "DELETE FROM projdescitem WHERE DescItemId = ?";
+	public static final String DELETEPROJDESCAITEMBYPROJECTDESCITEMID = "DELETE FROM projdescitem WHERE DescItemId = ?";
 
-	public static String FETCHUNIQUEITEMUNIT = "SELECT DISTINCT itemUnit FROM itemcodes";
+	public static final String FETCHUNIQUEITEMUNIT = "SELECT DISTINCT itemUnit FROM itemcodes";
 
-	public static String INSERTSUBPROJECTDESCRIPTION = "INSERT INTO projectDesc (ProjId, SubProjId,SerialNumber ,WorkType, Quantity, Metric, "
+	public static final String INSERTSUBPROJECTDESCRIPTION = "INSERT INTO projectDesc (ProjId, SubProjId,SerialNumber ,WorkType, Quantity, Metric, "
 			+ "Description, AliasDescription,LastUpdatedBy ,LastUpdatedAt) "
 			+ "VALUES (?, ? , ?, ?, ?, ?, ?, ?, ?, ?)";
 
-	public static String INSERTPROJECTDESCRIPTION = "INSERT INTO projectDesc (ProjId, SerialNumber, WorkType, Quantity, Metric, "
+	public static final String INSERTPROJECTDESCRIPTION = "INSERT INTO projectDesc (ProjId, SerialNumber, WorkType, Quantity, Metric, "
 			+ "Description, AliasDescription, LastUpdatedBy, LastUpdatedAt, PricePerQuantity, TotalCost) "
 			+ "VALUES (?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?)";
 
-	public static String INSERTGOVPROJECTDESCRIPTION = "INSERT INTO quotedprojectdesc (ProjId, SerialNumber, WorkType, Quantity, Metric, "
+	public static final String INSERTGOVPROJECTDESCRIPTION = "INSERT INTO quotedprojectdesc (ProjId, SerialNumber, WorkType, Quantity, Metric, "
 			+ "Description, AliasDescription, LastUpdatedBy, LastUpdatedAt, PricePerQuantity, TotalCost) "
 			+ "VALUES (?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?)";
 
-	public static String NOOFPROJECTDESCASSOCIATEDTOPROJECT = "SELECT count(*) FROM projectdesc WHERE ProjId = ?";
+	public static final String NOOFPROJECTDESCASSOCIATEDTOPROJECT = "SELECT count(*) FROM projectdesc WHERE ProjId = ?";
 	
-	public static String NOOFQUOTEDPROJECTDESCASSOCIATEDTOPROJECT = "SELECT count(*) FROM quotedprojectdesc WHERE ProjId = ?";
+	public static final String NOOFQUOTEDPROJECTDESCASSOCIATEDTOPROJECT = "SELECT count(*) FROM quotedprojectdesc WHERE ProjId = ?";
 
-	public static String NOOFPROJECTDESCASSOCIATEDTOSUBPROJECT = "SELECT count(*) FROM projectdesc WHERE SubProjId = ?";
+	public static final String NOOFPROJECTDESCASSOCIATEDTOSUBPROJECT = "SELECT count(*) FROM projectdesc WHERE SubProjId = ?";
 	
-	public static String NOOFQUOTEDPROJECTDESCASSOCIATEDTOSUBPROJECT = "SELECT count(*) FROM quotedprojectdesc WHERE SubProjId = ?";
+	public static final String NOOFQUOTEDPROJECTDESCASSOCIATEDTOSUBPROJECT = "SELECT count(*) FROM quotedprojectdesc WHERE SubProjId = ?";
 
-	public static String SAVEITEMS = "INSERT INTO itemcodes (itemName, itemUnit ,itemType) VALUES (?, ?, ?)";
+	public static final String SAVEITEMS = "INSERT INTO itemcodes (itemName, itemUnit ,itemType) VALUES (?, ?, ?)";
 
-    public static String UPDATEBASEDESCRIPTION ="UPDATE basedesc set WorkType = ?, Metric = ?, LastUpdatedBy = ?, LastUpdatedAt = ?, Description= ? where BaseDescription = ? ";
+    public static final String UPDATEBASEDESCRIPTION ="UPDATE basedesc set WorkType = ?, Metric = ?, LastUpdatedBy = ?, LastUpdatedAt = ?, Description= ? where BaseDescription = ? ";
 
-	public static String DEACTIVATEEXISTINGPRICES = "UPDATE pskpricedetail SET active ='0' WHERE projectId = ? AND subProjectId = ?";
+	public static final String DEACTIVATEEXISTINGPRICES = "UPDATE pskpricedetail SET active ='0' WHERE projectId = ? AND subProjectId = ?";
 
-	public static String INSERTPRICEFORITEMS = "INSERT INTO pskpricedetail (projectId, subProjectId, itemName, itemUnit, itemPrice, itemType, priceFeed) "
+	public static final String INSERTPRICEFORITEMS = "INSERT INTO pskpricedetail (projectId, subProjectId, itemName, itemUnit, itemPrice, itemType, priceFeed) "
 			+ "VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-	public static String INSERTBASEDESCRIPTION = "INSERT INTO basedesc(Category, WorkType, Metric, Quantity, PricePerQuantity , LastUpdatedBy, LastUpdatedAt, Description, BaseDescription) values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	public static final String INSERTBASEDESCRIPTION = "INSERT INTO basedesc(Category, WorkType, Metric, Quantity, PricePerQuantity , LastUpdatedBy, LastUpdatedAt, Description, BaseDescription) values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-	public static String FETCHBASEDESCRIPTIONS = "SELECT * FROM basedesc";
+	public static final String FETCHBASEDESCRIPTIONS = "SELECT * FROM basedesc";
 
-	public static String DELETEBASEDESCRIPTION = "DELETE FROM basedesc WHERE BaseDescId = ?";
+	public static final String DELETEBASEDESCRIPTION = "DELETE FROM basedesc WHERE BaseDescId = ?";
 
-    public static String ISBASEDESCEXISTS = "SELECT * FROM basedesc WHERE BaseDescription = ?";
+    public static final String ISBASEDESCEXISTS = "SELECT * FROM basedesc WHERE BaseDescription = ?";
 
-    public static String GETBASEDESCRIPTION = "SELECT * FROM basedesc WHERE BaseDescId = ?";
+    public static final String GETBASEDESCRIPTION = "SELECT * FROM basedesc WHERE BaseDescId = ?";
 
-    public static String INSERT_ITEM_RATE_DESCRIPTION = "INSERT INTO govpricedetail (itemName, itemUnit, itemType, itemPrice, priceFeed,scheduledItemNumber,itemDescription) "
+    public static final String INSERT_ITEM_RATE_DESCRIPTION = "INSERT INTO govpricedetail (itemName, itemUnit, itemType, itemPrice, priceFeed,scheduledItemNumber,itemDescription) "
             + "VALUES (?, ?, ?, ?, ?, ?, ?)";
 
 
-    public static String SAVE_ROLES = "INSERT INTO teamRole (teamName, roleName) VALUES (?, ?)";
+    public static final String SAVE_ROLES = "INSERT INTO teamRole (teamName, roleName) VALUES (?, ?)";
 
-    public static String GET_ALL_EMPLOYEE = "SELECT * FROM employee where empTeam != 'Admin'";
+    public static final String GET_ALL_EMPLOYEE = "SELECT * FROM employee where empTeam != 'Admin'";
 
-    public static String DELETE_EMPLOYEE = "DELETE FROM employee WHERE empId = ? ";
-
-
-    public static String GET_ROLES = "select roleName from teamRole where teamName = ?";
+    public static final String DELETE_EMPLOYEE = "DELETE FROM employee WHERE empId = ? ";
 
 
-    public static String GET_DROP_DOWN_VALUES = "select Value from pmsmastertable where Type = ?";
+    public static final String GET_ROLES = "select roleName from teamRole where teamName = ?";
+
+
+    public static final String GET_DROP_DOWN_VALUES = "select Value from pmsmastertable where Type = ?";
 
 
 }
