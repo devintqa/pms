d92c6e9d234e0767f564cfca678d29a69ab483b4
sunil <richiskumar@gmail.com>
2015-10-06 23:15:17 +0530

    D:5 Store functionality implementation

--- src/main/java/com/psk/pms/controller/StoreController.java
+++ src/main/java/com/psk/pms/controller/StoreController.java
@@ -20,7 +20,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.support.SessionStatus;
 
 import com.google.gson.Gson;
+import com.psk.pms.model.DescItemDetail;
 import com.psk.pms.model.JsonData;
+import com.psk.pms.model.ProjDescComparisonDetail;
+import com.psk.pms.model.ProjectConfiguration;
+import com.psk.pms.model.ProjectConfiguration.ItemDetail;
+import com.psk.pms.model.ProjectItemDescription;
+import com.psk.pms.model.StockDetail;
 import com.psk.pms.model.StoreDetail;
 import com.psk.pms.service.StoreService;
 import com.psk.pms.validator.StoreValidator;
@@ -31,72 +37,114 @@ import com.psk.pms.validator.StoreValidator;
 @Controller
 public class StoreController extends BaseController {
 
-    private static final Logger LOGGER = Logger
-            .getLogger(SearchController.class);
+	private static final Logger LOGGER = Logger
+			.getLogger(SearchController.class);
 
-    @Autowired
-    private StoreValidator storeValidator;
+	@Autowired
+	private StoreValidator storeValidator;
 
-    @Autowired
-    private StoreService storeService;
+	@Autowired
+	private StoreService storeService;
 
-    @RequestMapping(value = "/emp/myview/buildStoreDetail/{employeeId}", method = RequestMethod.GET)
-    public String buildStoreDetail(@PathVariable String employeeId, Model model) {
-        LOGGER.info("Store Controller : buildStoreDetail()");
-        Map<String, String> aliasProjectList = getProjectDetails(employeeId);
-        StoreDetail storeDetail = new StoreDetail();
-        storeDetail.setEmployeeId(employeeId);
-        model.addAttribute("storeDetailForm", storeDetail);
-        model.addAttribute("aliasProjectList", aliasProjectList);
-        return "BuildStoreDetail";
-    }
+	@RequestMapping(value = "/emp/myview/buildStoreDetail/{employeeId}", method = RequestMethod.GET)
+	public String buildStoreDetail(@PathVariable String employeeId, Model model) {
+		LOGGER.info("Store Controller : buildStoreDetail()");
+		Map<String, String> aliasProjectList = getProjectDetails(employeeId);
+		StoreDetail storeDetail = new StoreDetail();
+		storeDetail.setEmployeeId(employeeId);
+		model.addAttribute("storeDetailForm", storeDetail);
+		model.addAttribute("aliasProjectList", aliasProjectList);
+		return "BuildStoreDetail";
+	}
 
-    @RequestMapping(value = "/emp/myview/buildStoreDetail/getItemNames.do", method = RequestMethod.GET)
-    @ResponseBody
-    public JsonData getItemNames(HttpServletRequest httpServletRequest) {
-        LOGGER.info("getItemNames for Item Type :" + httpServletRequest.getParameter("itemType"));
-        JsonData jsonData = new JsonData();
-        String itemType = httpServletRequest.getParameter("itemType");
-        String projId = httpServletRequest.getParameter("projId");
-        String employeeId = httpServletRequest.getParameter("employeeId");
-        String projectId = fetchProjectId(projId, employeeId);
-        List<String> itemNames = itemService.getItemNames(itemType, projectId);
-        if (!itemNames.isEmpty()) {
-            Gson gson = new Gson();
-            jsonData.setData(gson.toJson(itemNames));
-            jsonData.setSuccess(true);
-        }
-        return jsonData;
-    }
+	@RequestMapping(value = "/emp/myview/buildStoreDetail/getItemNames.do", method = RequestMethod.GET)
+	@ResponseBody
+	public JsonData getItemNames(HttpServletRequest httpServletRequest) {
+		LOGGER.info("getItemNames for Item Type :"
+				+ httpServletRequest.getParameter("itemType"));
+		JsonData jsonData = new JsonData();
+		String itemType = httpServletRequest.getParameter("itemType");
+		String projId = httpServletRequest.getParameter("projId");
+		String employeeId = httpServletRequest.getParameter("employeeId");
+		String projectId = fetchProjectId(projId, employeeId);
+		List<String> itemNames = itemService.getItemNames(itemType, projectId);
+		if (!itemNames.isEmpty()) {
+			Gson gson = new Gson();
+			jsonData.setData(gson.toJson(itemNames));
+			jsonData.setSuccess(true);
+		}
+		return jsonData;
+	}
 
-    @RequestMapping(value = "/emp/myview/buildStoreDetail/saveStoreDetail.do", method = RequestMethod.POST)
-    public String saveStoreDetail(@ModelAttribute("storeDetailForm") StoreDetail storeDetail,
-                                    BindingResult result, Model model, SessionStatus status) {
-        try {
-            Map<String, String> aliasProjectList = getProjectDetails(storeDetail.getEmployeeId());
-            storeValidator.validate(storeDetail, result);
-            model.addAttribute("storeDetailForm", storeDetail);
-            model.addAttribute("aliasProjectList", aliasProjectList);
-            if (result.hasErrors()) {
-                return "BuildStoreDetail";
-            }
-            storeService.saveStoreDetail(storeDetail);
-            model.addAttribute("successMessage", "Store Details saved successfully");
-        } catch (Exception e) {
-            model.addAttribute("errorMessage", e.getMessage());
-        }
-        return "BuildStoreDetail";
+	
+	
 
-    }
+	@RequestMapping(value = "/emp/myview/dispatchTransaction/getItemNamesInStore.do", method = RequestMethod.GET)
+	@ResponseBody
+	public JsonData getItemNamesInStore(HttpServletRequest httpServletRequest) {
+		LOGGER.info("getItemNamesInStore for Project :"
+				+ httpServletRequest.getParameter("projId"));
+		JsonData jsonData = new JsonData();
+		String projId = httpServletRequest.getParameter("projId");
+		String employeeId = httpServletRequest.getParameter("employeeId");
+		String projectId = fetchProjectId(projId, employeeId);
+		List<String> itemNames = storeService.getItemNamesInStore(projId);
+		if (!itemNames.isEmpty()) {
+			Gson gson = new Gson();
+			jsonData.setData(gson.toJson(itemNames));
+			jsonData.setSuccess(true);
+		}
+		return jsonData;
+	}
+	
 
-    private Map<String, String> getProjectDetails(String empId) {
-        return populateAliasProjectList(empId);
-    }
+	
+	
+	@RequestMapping(value = "/emp/myview/dispatchTransaction/{employeeId}", method = RequestMethod.GET)
+	public String dispatchTransaction(@PathVariable String employeeId, Model model) {
+		LOGGER.info("Store Controller : buildStoreDetail()");
+		Map<String, String> aliasProjectList = getProjectDetails(employeeId);
+		StoreDetail storeDetail = new StoreDetail();
+		storeDetail.setEmployeeId(employeeId);
+		model.addAttribute("storeDetailForm", storeDetail);
+		model.addAttribute("aliasProjectList", aliasProjectList);
+		return "StoreTransaction";
+	}
 
-    @ModelAttribute("itemTypes")
-    private List<String> getItemTypes() {
-        List<String> itemTypes = projectService.getDropDownValuesFor(ITEM_TYPE);
-        itemTypes.add(0, "--Please Select--");
-        return itemTypes;
-    }
+	
+	
+	
+	@RequestMapping(value = "/emp/myview/buildStoreDetail/saveStoreDetail.do", method = RequestMethod.POST)
+	public String saveStoreDetail(
+			@ModelAttribute("storeDetailForm") StoreDetail storeDetail,
+			BindingResult result, Model model, SessionStatus status) {
+		try {
+			Map<String, String> aliasProjectList = getProjectDetails(storeDetail
+					.getEmployeeId());
+			storeValidator.validate(storeDetail, result);
+			model.addAttribute("storeDetailForm", storeDetail);
+			model.addAttribute("aliasProjectList", aliasProjectList);
+			if (result.hasErrors()) {
+				return "BuildStoreDetail";
+			}
+			storeService.saveStoreDetail(storeDetail);
+			model.addAttribute("successMessage",
+					"Store Details saved successfully");
+		} catch (Exception e) {
+			model.addAttribute("errorMessage", e.getMessage());
+		}
+		return "BuildStoreDetail";
+
+	}
+
+	private Map<String, String> getProjectDetails(String empId) {
+		return populateAliasProjectList(empId);
+	}
+
+	@ModelAttribute("itemTypes")
+	private List<String> getItemTypes() {
+		List<String> itemTypes = projectService.getDropDownValuesFor(ITEM_TYPE);
+		itemTypes.add(0, "--Please Select--");
+		return itemTypes;
+	}
 }
--- src/main/java/com/psk/pms/service/StoreService.java
+++ src/main/java/com/psk/pms/service/StoreService.java
@@ -1,14 +1,18 @@
 package com.psk.pms.service;
 
-import com.psk.pms.model.StoreDetail;
-
 import java.util.List;
+
+import com.psk.pms.model.StockDetail;
+import com.psk.pms.model.StoreDetail;
 
 /**
  * Created by Sony on 26-09-2015.
  */
 public interface StoreService {
+	
     void saveStoreDetail(StoreDetail storeDetail);
 
     List<StoreDetail> getStoreDetails(int projId);
+    
+    List<String> getItemNamesInStore(String projId);
 }
